b7904f9f0d991b2839ea438d68bf72c6a87b0dac
Erik Ccanto <ccanto.erik@gmail.com>
2023-03-27 18:40:55 -0300

    refactor: code style

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from "cypress"
-import createBundler from "@bahmutov/cypress-esbuild-preprocessor"
-import preprocessor from "@badeball/cypress-cucumber-preprocessor"
-import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild.js"
+import { defineConfig } from 'cypress'
+import createBundler from '@bahmutov/cypress-esbuild-preprocessor'
+import preprocessor from '@badeball/cypress-cucumber-preprocessor'
+import createEsbuildPlugin from '@badeball/cypress-cucumber-preprocessor/esbuild.js'
 
 export async function setupNodeEvents(on, config) {
   await preprocessor.addCucumberPreprocessorPlugin(on, config)
 
   on(
-    "file:preprocessor",
+    'file:preprocessor',
     createBundler({
       plugins: [createEsbuildPlugin.default(config)],
     })
@@ -18,7 +18,7 @@ export async function setupNodeEvents(on, config) {
 
 export default defineConfig({
   e2e: {
-    specPattern: "**/*.feature",
+    specPattern: '**/*.feature',
     setupNodeEvents,
   },
 })
